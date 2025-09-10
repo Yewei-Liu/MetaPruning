d871e817ba4e196200a9327931b728850a64e262
Yewei-Liu <2300012959@stu.pku.edu.cn>
2025-09-10 07:05:10 +0000

    20250910

--- main_imagenet/scripts/resnet50/finetune.sh
+++ main_imagenet/scripts/resnet50/finetune.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH -J linkpred_by_liuyewei
+#SBATCH -J liuyewei
 #SBATCH -p IAI_SLURM_HGX
 #SBATCH --qos=16gpu-hgx
 #SBATCH -N 1
 #SBATCH --gres=gpu:8
-#SBATCH --time=96:00:00
+#SBATCH --time=48:00:00
 #SBATCH -c 8
 #SBATCH -o linkpred.out
 #SBATCH -e linkpred.err
