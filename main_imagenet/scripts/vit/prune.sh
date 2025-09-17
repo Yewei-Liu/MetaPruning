5870e8d87d541250c13166ddada44bce185ee458
Yewei-Liu <2300012959@stu.pku.edu.cn>
2025-09-17 15:31:15 +0000

    20250917

---
+++ main_imagenet/scripts/vit/prune.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#SBATCH -J metapruning
+#SBATCH -p IAI_SLURM_3090
+#SBATCH --qos=8gpu
+#SBATCH -N 1
+#SBATCH --gres=gpu:1
+#SBATCH --time=48:00:00
+#SBATCH -c 64
+#SBATCH -o tmp.out
+#SBATCH -e tmp.err
+
+
+~/.conda/envs/MetaPruning/bin/python -c "import torch; print(torch.__version__)"
+~/.conda/envs/MetaPruning/bin/python -c "import torch; print(torch.cuda.is_available())"
+~/.conda/envs/MetaPruning/bin/python -c "import torch; print(torch.cuda.device_count())"
+
+
+
+MODEL="vit_b_16"
+INDEX=0
+RUN_TYPE="prune"               
+SPEED_UP=1.1
+NAME="Final_ViT"
+PRETRAINED=True
+DEVICE=0      
+CONFIG_NAME="base"              
+        
+export HYDRA_FULL_ERROR=1
+
+mkdir -p "save/${NAME}/${INDEX}/${RUN_TYPE}"
+
+python main_imagenet.py \
+    +experiment=$CONFIG_NAME \
+    model=$MODEL \
+    no_distribution=True \
+    run=$RUN_TYPE \
+    speed_up=$SPEED_UP \
+    index=$INDEX \
+    name=$NAME \
+    pretrained=$PRETRAINED \
+    > "save/${NAME}/${INDEX}/${RUN_TYPE}/${SPEED_UP}.log" 2>&1
