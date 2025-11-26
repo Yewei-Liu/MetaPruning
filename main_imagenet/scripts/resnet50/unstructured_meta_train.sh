df041f206c1d51b7d155caa9981a0e5a00c75ff9
Yewei-Liu <2300012959@stu.pku.edu.cn>
2025-11-26 14:07:54 +0000

    20251126

---
+++ main_imagenet/scripts/resnet50/unstructured_meta_train.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+#SBATCH -J metapruning
+#SBATCH -p IAI_SLURM_HGX
+#SBATCH --qos=16gpu-hgx
+#SBATCH -N 1
+#SBATCH --gres=gpu:8
+#SBATCH --time=48:00:00
+#SBATCH -c 64
+#SBATCH -o meta_train.out
+#SBATCH -e meta_train.err
+
+
+~/.conda/envs/MetaPruning/bin/python -c "import torch; print(torch.__version__)"
+~/.conda/envs/MetaPruning/bin/python -c "import torch; print(torch.cuda.is_available())"
+~/.conda/envs/MetaPruning/bin/python -c "import torch; print(torch.cuda.device_count())"
+
+METHOD=unstructured_l1_norm
+PRUNER_REG=0.1
+
+MODEL="resnet50"  
+DATA_MODEL_NUM=2
+RUN_TYPE="meta_train"     
+NAME=FinalResnet
+RESUME_EPOCH=-1
+# metanetwork
+NUM_LAYER=6
+HIDDIM=16
+IN_NODE_DIM=8
+NODE_RES_RATIO=0.002
+EDGE_RES_RATIO=0.002
+METATRAIN_LR_DECAY_MILESTONES=\'2\'
+
+NUM_GPUS=8
+MASTER_PORT=18900             
+CONFIG_NAME="base"              
+        
+
+# Find available port
+while true; do
+    if ! nc -z 127.0.0.1 $MASTER_PORT; then
+        break
+    fi
+    ((MASTER_PORT++))
+done
+
+export HYDRA_FULL_ERROR=1
+export OMP_NUM_THREADS=4
+export NCCL_DEBUG=WARN
+export TORCH_DISTRIBUTED_DEBUG=INFO
+
+mkdir -p "save/${NAME}/${RUN_TYPE}"
+mkdir -p "save/${NAME}/${RUN_TYPE}/metanetwork"
+# save/Maybe/0/meta_train/metanetwork 
+
+torchrun \
+    --nproc_per_node=$NUM_GPUS \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr="127.0.0.1" \
+    --master_port=$MASTER_PORT \
+    main_imagenet.py \
+    +experiment=$CONFIG_NAME \
+    method=$METHOD \
+    meta_train.pruner_reg=$PRUNER_REG \
+    metanetwork.num_layer=$NUM_LAYER \
+    metanetwork.hiddim=$HIDDIM \
+    metanetwork.in_node_dim=$IN_NODE_DIM \
+    metanetwork.node_res_ratio=$NODE_RES_RATIO \
+    metanetwork.edge_res_ratio=$EDGE_RES_RATIO \
+    meta_train.lr_decay_milestones=$METATRAIN_LR_DECAY_MILESTONES \
+    model=$MODEL \
+    run=$RUN_TYPE \
+    data_model_num=$DATA_MODEL_NUM \
+    resume_epoch=$RESUME_EPOCH \
+    name=$NAME \
+    > "save/${NAME}/${RUN_TYPE}/train.log" 2>&1
